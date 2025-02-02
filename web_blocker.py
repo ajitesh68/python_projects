3a87dd0a973e3f4b2d4b86e9be64b1e1fe3bd8cf
ajitesh68 <ajiteshshukla1234@gmail.com>
2025-02-02 18:24:17 +0530

    updated

--- web_blocker.py
+++ web_blocker.py
@@ -1,34 +1,49 @@
 import time 
-from datetime import datetime as dt
+from datetime import datetime as dt  # Import datetime module to handle time
 
+# IP address used for redirecting blocked websites (localhost)
 ip_locatime = "127.0.0.1"
-website_list=["www.facebook.com","www.instagram.com"]
 
-host_path="/etc/hosts"
-startime="09.0.0"
-endtime="18.0.0"
+# List of websites to be blocked
+website_list = ["www.facebook.com", "www.instagram.com"]
 
-now=dt.now()
-current_time=now.strftime("%H:%H:%S")
-print(current_time)
+# Path to the system's hosts file (used for blocking websites)
+host_path = "/etc/hosts"
+
+# Define working hours (blocking time) using datetime objects
+startime = dt.strptime("09:00:00", "%H:%M:%S").time()  # Start blocking at 9:00 AM
+endtime = dt.strptime("21:00:00", "%H:%M:%S").time()    # Stop blocking at 9:00 PM
+
+# Infinite loop to continuously check the time and block/unblock websites
 while True:
-    if startime<=current_time and current_time<=endtime:
-        print("working hours")
-        with open(host_path,"r+") as file:
-            content = file.read()
+    now = dt.now().time()  # Get current system time dynamically
+    print(f"Current time: {now}")  # Print current time for debugging
+
+    # If current time is within the blocking hours
+    if startime <= now <= endtime:
+        print("Working hours: Blocking websites")
+
+        # Open the hosts file in read & write mode ('r+')
+        with open(host_path, "r+") as file:
+            content = file.read()  # Read the entire file content
+
+            # Check if websites are already blocked, if not, block them
             for website in website_list:
-                if website in content:
-                    pass
-                else:
-                    file.write(ip_locatime+" "+website+"\n")
+                if website not in content:  # If website entry is not in the file
+                    file.write(ip_locatime + " " + website + "\n")  # Add to hosts file
     else:
-        print("Non working hour")
-        with open(host_path,"r+") as file:
-            content = file.readlines()
-            file.seek(0)
+        print("Non-working hours: Unblocking websites")
+
+        # Open the hosts file in read & write mode
+        with open(host_path, "r+") as file:
+            content = file.readlines()  # Read all lines into a list
+            file.seek(0)  # Move cursor to the beginning of the file
+
+            # Write back only lines that do not contain blocked websites
             for line in content:
                 if not any(website in line for website in website_list):
                     file.write(line)
 
-                file.truncate()                    
-    time.sleep(10)
+            file.truncate()  # Truncate the file to remove extra data
+
+    time.sleep(10)  # Wait for 10 seconds before checking again
